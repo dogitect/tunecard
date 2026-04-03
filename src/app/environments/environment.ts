a46d9071b289ba8bf5455ae4470219a70bf5888f
Leon Xia <253962588+dogitect@users.noreply.github.com>
2026-04-04 04:42:20 +0800

    chore: add environment.ts for development environment configuration

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,11 @@
+/**
+ * @license
+ * Copyright 2026 Leon Xia. All rights reserved.
+ * SPDX-License-Identifier: MIT
+ */
+
+/** Development environment configuration. */
+export const environment = {
+  production: false,
+  workerBaseUrl: 'http://localhost:8787/api',
+};
