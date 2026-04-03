2f67cf2e433e08b29136fdeae19bc7ecdf527e2f
Leon Xia <253962588+dogitect@users.noreply.github.com>
2026-04-04 04:42:17 +0800

    chore: add environment.prod.ts for production environment configuration

---
+++ src/app/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+/**
+ * @license
+ * Copyright 2026 Leon Xia. All rights reserved.
+ * SPDX-License-Identifier: MIT
+ */
+
+/** Production environment configuration. */
+export const environment = {
+  production: true,
+  workerBaseUrl: '/api',
+};
